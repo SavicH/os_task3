6d6de1f06e1f8bf36a5e3fb79bc5d8b93e5d12d5
Savvin Sergey <sav1ch@hotmail.com>
2013-12-12 12:06:57 +0400

    Fuse functions prototypes

--- fat.c
+++ fat.c
@@ -2,7 +2,73 @@
 
 #include <fuse.h>
 
-static struct fuse_operations fat_oper;
+int fat_open(const char *path, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+int fat_create(const char *path, mode_t mode, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+
+int fat_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+int fat_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+int fat_truncate(const char *path, off_t offset)
+{
+	return 0;
+}
+
+int fat_release(const char *path, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+int fat_flush(const char *path, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+int fat_unlink(const char *path)
+{
+	return 0;
+}
+
+int fat_getattr(const char *path, struct stat *stat)
+{
+	return 0;
+}
+
+int fat_mkdir(const char *path, mode_t mode)
+{
+	return 0;
+}
+
+int fat_rmdir(const char *path)
+{
+	return 0;
+}		
+
+int fat_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *info)
+{
+	return 0;
+}
+
+static struct fuse_operations fat_oper = {
+    .getattr    = fat_getattr,
+    .readdir    = fat_readdir,
+    .open       = fat_open,
+    .read       = fat_read
+};
 
 int main(int argc, char *argv[])
 {
