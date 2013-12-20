55f4e531be7e683756a6049e8d32005ee3fc4347
Savvin Sergey <sav1ch@hotmail.com>
2013-12-20 09:08:56 +0400

    Added getattr and readdir functions

--- fat.c
+++ fat.c
@@ -1,38 +1,127 @@
+/*
+	clang -Wall fat.c `pkg-config fuse --cflags --libs` -o ./bin/fat
+*/
+
 #define FUSE_USE_VERSION 29
 
-#include <fuse.h>
 #include <errno.h>
+#include <fuse.h>
+#include <mcheck.h>
+#include <stdio.h>
+#include <stdlib.h>	
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
+
 
 #define IMAGE_FILE_NAME "data"
-#define NAME_LENGTH 19
+#define NAME_LENGTH 16
 #define CLUSTER_SIZE 512
 #define CLUSTER_COUNT 100
+#define END_OF_FILE 0xFFFFFFFF
+#define EMPTY 0x00000000
 
 typedef unsigned int cluster_t;
 
-struct fat_entry {
-	char *name[NAME_LENGTH];
-	char permissions;
+struct file_entry_s {
+	char name[NAME_LENGTH];
+	mode_t mode;
 	time_t created;
 	cluster_t cluster;
 	size_t size;
 };
 
-int data_offset;
+typedef struct file_entry_s file_entry;
+
+int image;
+off_t data_offset;
+int entries_count;
+
+char* extract_folder(const char *path) {
+	int length = strlen(path);
+	int i = length - 1;
+	while (i > 0 && path[i] != '/') {
+		--i;
+		
+	}
+	printf("Folder last '/' %d\n", i);
+	printf("start malloc folder\n");
+	char *result = (char *)malloc(length+1);
+	printf("finish malloc folder\n");
+	strcpy(result, path);
+	result[i] = '\0';	
+	printf("Folder: %s %d\n", result, strlen(result));
+	return result;
+}
+
+char* extract_filename(const char *path) {
+	int length = strlen(path);
+	int i = length - 1;
+		while (i > 0 && path[i] != '/') {
+		--i;
+	}
+	printf("File last '/' %d\n", i);
+	if (i < 0) {
+		return NULL;
+	}
+	printf("start malloc name\n");
+	char *result = (char *)malloc(length - i);
+	printf("finish malloc name\n");
+	strcpy(result, path + i + 1);
+	printf("Name: %s %d\n", result, strlen(result));
+	return result;
+}
+
+int get_file_entry_from_cluster(const char *name, cluster_t cluster, file_entry *file) {
+	printf("cluster %d\n", cluster);
+	lseek(image, data_offset + cluster * CLUSTER_SIZE, SEEK_SET);
+	int i;
+	file_entry tmp;
+	for (i = 0; i<entries_count; i++) {
+		read(image, &tmp, sizeof(file_entry));
+		if (strcmp(name, (char*)tmp.name) == 0) {
+			*file = tmp;
+			return 0;	
+		}
+	}
+	return 1;
+}
+
+int get_file_entry(const char *path, file_entry *file) {
+	printf("Path: %s\n", path);
+	if (strcmp(path, "") == 0) {
+		file_entry root;
+		root.cluster = 0;
+		root.mode = S_IFDIR | 0755;
+		*file = root;
+		return 0;
+	}
+	file_entry tmp;
+	if (get_file_entry(extract_folder(path), &tmp) != 0) {
+		return 1;
+	}
+	if (get_file_entry_from_cluster(extract_filename(path), tmp.cluster, file) != 0) {
+		return 1;
+	}
+	return 0;
+}
 
 int fat_getattr(const char *path, struct stat *stat) {
-	int res = 0;
-
 	memset(stat, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
 		stat->st_mode = S_IFDIR | 0755;
 		stat->st_nlink = 2;
-	} else
-		res = -ENOENT;
-
-	return res;
+	} else {
+		file_entry file;
+		if (get_file_entry(path, &file) != 0) {
+			return -ENOENT;
+		}
+		stat->st_mode = S_IFREG | 0444;
+		stat->st_nlink = 1;
+		stat->st_size = file.size;
+	}
+	return 0;
 }
 
 
@@ -43,59 +132,41 @@ int fat_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offse
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
+	lseek(image, data_offset, SEEK_SET);
+	int i;
+	file_entry tmp;
+	char *filename = malloc(NAME_LENGTH);
+	for (i = 0; i<entries_count; i++) {
+		read(image, &tmp, sizeof(file_entry));
+		if (tmp.mode != 0) {
+			memcpy(filename, tmp.name, NAME_LENGTH);
+			filler(buf, filename, NULL, 0);
+		} else {
+			break;
+		}
+	}
 	return 0;
 }
 
-int fat_open(const char *path, struct fuse_file_info *info) {
-	return 0;
-}
 
 int fat_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *info) {
 	return 0;
 }
 
-int fat_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *info) {
-	return 0;
-}
-
-int fat_create(const char *path, mode_t mode, struct fuse_file_info *info) {
-	return 0;
-}
-
-int fat_truncate(const char *path, off_t offset) {
-	return 0;
-}
-
-int fat_release(const char *path, struct fuse_file_info *info) {
-	return 0;
-}
-
-int fat_flush(const char *path, struct fuse_file_info *info) {
-	return 0;
-}
-
-int fat_unlink(const char *path) {
-	return 0;
-}
-
-
-
-int fat_mkdir(const char *path, mode_t mode) {
-	return 0;
-}
-
-int fat_rmdir(const char *path) {
-	return 0;
-}		
-
-
 static struct fuse_operations fat_oper = {
     .getattr    = fat_getattr,
     .readdir    = fat_readdir,
-    .open       = fat_open,
-    .read       = fat_read
+    .read       = fat_read,
+    .open 		= fat_open
 };
 
 int main(int argc, char *argv[]) {
+	image = open(IMAGE_FILE_NAME, O_RDWR);
+	if (image == -1) {
+		printf("Error!\n");
+		return 1;
+	}
+	data_offset = sizeof(cluster_t) * CLUSTER_COUNT;
+	entries_count = CLUSTER_SIZE / sizeof(file_entry);
 	return fuse_main(argc, argv, &fat_oper, NULL);
 }
